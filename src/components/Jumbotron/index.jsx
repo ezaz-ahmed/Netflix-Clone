a7f31fcb5df4aadb8741729726e5c5dcf149386f
Ezaz Ahmed <ezaznahmed@gmail.com>
2021-09-04 21:23:20 +0600

    first commit

---
+++ src/components/Jumbotron/index.jsx
@@ -0,0 +1,39 @@
+import {
+  Container,
+  Item,
+  Inner,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbrotron';
+
+const Jumbotron = ({ children, direction, ...restProps }) => {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+};
+
+Jumbotron.Container = ({ children, ...restProps }) => {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = ({ children, ...restProps }) => {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = ({ children, ...restProps }) => {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = ({ children, ...restProps }) => {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = ({ ...restProps }) => {
+  return <Image {...restProps} />;
+};
+
+export default Jumbotron;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import { JumbotronContainer } from '../containers/Jumbotron';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <JumbotronContainer />
+    </Fragment>
+  );
+};
+
+export default Home;
